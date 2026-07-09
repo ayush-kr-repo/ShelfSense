38af687c19b9e3dfff16a277d78ba1fb76cb3344
ayush-kr-repo <ayushmvm9@gmail.com>
2026-07-09 19:50:50 +0530

    Remove local database from repo

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 # The connection string : "talk SQLite, to the file ./shelfsense.db"
-engine = create_engine("sqlite:///.shelfsense.db",
+engine = create_engine("sqlite:///./shelfsense.db",
                        connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(bind=engine)
